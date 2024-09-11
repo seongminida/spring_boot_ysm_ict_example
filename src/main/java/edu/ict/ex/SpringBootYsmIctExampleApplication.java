8f54ebde910f13a79bc39be236c0894da5be584d
WIN-G0RS8KPELJQ\ICT02_17 <mcytds@naver.com>
2024-09-11 11:34:55 +0900

    첫번쨰 커밋 - 스프링 기본 설정

---
+++ src/main/java/edu/ict/ex/SpringBootYsmIctExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootYsmIctExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootYsmIctExampleApplication.class, args);
+	}
+
+}
